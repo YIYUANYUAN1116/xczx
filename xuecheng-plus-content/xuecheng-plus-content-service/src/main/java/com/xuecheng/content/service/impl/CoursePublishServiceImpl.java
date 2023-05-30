f01044c41e5f07c711c6028f790c03e6cac71879
YZD <571145417@qq.com>
2023-05-30 20:13:40 +0800

    fourth 4.4

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CoursePublishServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CoursePublishServiceImpl.java
@@ -3,21 +3,18 @@ package com.xuecheng.content.service.impl;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.xuecheng.base.execption.XueChengException;
-import com.xuecheng.content.mapper.CourseBaseMapper;
-import com.xuecheng.content.mapper.CourseMarketMapper;
-import com.xuecheng.content.mapper.CoursePublishPreMapper;
+import com.xuecheng.content.mapper.*;
 import com.xuecheng.content.model.dto.CourseBaseInfoDto;
 import com.xuecheng.content.model.dto.CoursePreviewDto;
 import com.xuecheng.content.model.dto.TeachplanDto;
-import com.xuecheng.content.model.po.CourseBase;
-import com.xuecheng.content.model.po.CourseMarket;
-import com.xuecheng.content.model.po.CoursePublishPre;
+import com.xuecheng.content.model.po.*;
 import com.xuecheng.content.service.CourseBaseInfoService;
 import com.xuecheng.content.service.CoursePublishService;
 import com.xuecheng.content.service.TeachplanService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
@@ -41,6 +38,13 @@ public class CoursePublishServiceImpl implements CoursePublishService {
 
     @Resource
     CourseBaseMapper courseBaseMapper;
+
+    @Resource
+    CoursePublishMapper coursePublishMapper;
+
+    @Resource
+    MqMessageMapper mqMessageMapper;
+
     @Override
     public CoursePreviewDto getCoursePreviewInfo(Long courseId) {
         //获取课程基本信息，营销信息
@@ -111,6 +115,84 @@ public class CoursePublishServiceImpl implements CoursePublishService {
         //更新课程基本表的审核状态
         courseBase.setAuditStatus("202003");
         courseBaseMapper.updateById(courseBase);
+
+    }
+
+    @Transactional
+    @Override
+    public void publish(Long companyId, Long courseId) {
+
+        CoursePublishPre coursePublishPre = coursePublishPreMapper.selectById(courseId);
+        //约束
+        if(coursePublishPre == null){
+            XueChengException.cast("请先提交课程审核，审核通过才可以发布");
+        }
+        //1.本机构只允许提交本机构的课程
+        if(!coursePublishPre.getCompanyId().equals(companyId)){
+            XueChengException.cast("不允许提交其它机构的课程。");
+        }
+
+        //2.课程审核状态
+        String auditStatus = coursePublishPre.getStatus();
+        //3.审核通过方可发布
+        if(!"202004".equals(auditStatus)){
+            XueChengException.cast("操作失败，课程审核通过方可发布。");
+        }
+
+
+        //课程发布操作对数据库操作如下：
+        //1、向课程发布表course_publish插入一条记录,记录来源于课程预发布表，如果存在则更新，发布状态为：已发布。
+        //2、更新course_base表的课程发布状态为：已发布
+        //3、删除课程预发布表的对应记录。
+        //4、向mq_message消息表插入一条消息，消息类型为：course_publish
+
+        //保存课程发布信息
+        saveCoursePublish(courseId);
+
+        //保存消息表
+        saveCoursePublishMessage(courseId);
+
+        //删除课程预发布表对应记录
+        coursePublishPreMapper.deleteById(courseId);
+
+    }
+
+    /**
+     * 保存课程发布信息
+     * @param courseId
+     */
+    private void saveCoursePublish(Long courseId) {
+        //整合课程发布信息
+        //查询课程预发布表
+        CoursePublishPre coursePublishPre = coursePublishPreMapper.selectById(courseId);
+        if(coursePublishPre == null){
+            XueChengException.cast("课程预发布数据为空");
+        }
+
+        CoursePublish coursePublish = new CoursePublish();
+        BeanUtils.copyProperties(coursePublishPre,coursePublish);
+        coursePublish.setStatus("203002");
+        CoursePublish coursePublishUpdate = coursePublishMapper.selectById(courseId);
+        if (coursePublishUpdate == null){
+            //插入
+            coursePublishMapper.insert(coursePublish);
+        }else {
+            //更新
+            coursePublishMapper.updateById(coursePublish);
+        }
+
+        //跟新基本课程表
+        CourseBase courseBase = courseBaseMapper.selectById(courseId);
+        courseBase.setStatus("203002");
+        courseBaseMapper.updateById(courseBase);
+
+    }
+
+    /**
+     * 保存消息表
+     * @param courseId
+     */
+    private void saveCoursePublishMessage(Long courseId) {
 
     }
 }
